62980b958ba12087bed9cc572e72a6d8993632ff
Tor Shimizu <torshimizu@gmail.com>
2018-03-06 16:40:59 -0800

    can create new instance of Hotel::Block

--- lib/block.rb
+++ lib/block.rb
@@ -1,4 +1,12 @@
 module Hotel
   class Block
+    attr_reader :start_date, :end_date, :block_rooms, :rate
+    
+    def initialize(input)
+      @start_date = input[:start_date]
+      @end_date = input[:end_date]
+      @block_rooms = input[:block_rooms]
+      @rate = input[:rate]
+    end
   end
 end
--- specs/block_spec.rb
+++ specs/block_spec.rb
@@ -1,1 +1,20 @@
 require_relative 'spec_helper'
+
+describe 'Hotel::Block'do
+
+  describe 'Block#initialize'do
+    before do
+      @number_of_rooms = 20
+      @admin = Hotel::Admin.new(@number_of_rooms)
+      block_rooms = @admin.rooms.select {|room| (1..5).include?(room.room_id)}
+      input = {start_date: "2018-03-08", end_date: "2018-03-12", block_rooms: block_rooms, rate: 150}
+      @new_block = Hotel::Block.new(input)
+    end
+
+    it "can be instantiated" do
+      @new_block.must_be_instance_of Hotel::Block
+    end
+
+  end
+
+end
